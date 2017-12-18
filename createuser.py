8501c9077327cd61ee7c2e006856d33a6689f2ae
RyQcan <lee.yanzhe@yanzhe.org>
2017-12-18 09:21:15 +0800

    createuser

--- createuser.py
+++ createuser.py
@@ -1,11 +1,9 @@
-from django.conf import settings
 from django.contrib.auth.models import User
 
 namelist = ['lintianxiang', 'jiaozhengang', 'liyanzhe', 'zhangdachuan',
             'fuyao', 'jinzhen', 'lihuaxin', 'lisirui', 'linjinxiu', 'huangxin',
             'wangxiaowei', 'wangyang', 'yangjiageng', 'zhanghanwen', 'zhangzundong',
             'leipengqun', 'liujiahao', 'wangxuwu']
-if not settings.configured:
-    settings.configure()
+
 for name in namelist:
     user = User.objects.create_user(name, '123@123.com', name)
