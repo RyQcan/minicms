23bb2ef14d033da27aa1f71c4e13e4c5c8194b8c
RyQcan <13723926068@163.com>
2017-12-18 09:11:15 +0800

    creatuser
    creatuser

---
+++ createuser.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+
+namelist=['lintianxiang','jiaozhengang','liyanzhe','zhangdachuan',
+'fuyao','jinzhen','lihuaxin','lisirui','linjinxiu','huangxin',
+'wangxiaowei','wangyang','yangjiageng','zhanghanwen','zhangzundong',
+'leipengqun','liujiahao','wangxuwu']
+for name in namelist:
+    user = User.objects.create_user(name, '123@123.com', name)
+
