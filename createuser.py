7d14099a60fa67f4f5fac6c296fde4c807c1b16a
RyQcan <lee.yanzhe@yanzhe.org>
2017-12-18 09:17:16 +0800

    createuser

--- createuser.py
+++ createuser.py
@@ -5,7 +5,6 @@ namelist = ['lintianxiang', 'jiaozhengang', 'liyanzhe', 'zhangdachuan',
             'fuyao', 'jinzhen', 'lihuaxin', 'lisirui', 'linjinxiu', 'huangxin',
             'wangxiaowei', 'wangyang', 'yangjiageng', 'zhanghanwen', 'zhangzundong',
             'leipengqun', 'liujiahao', 'wangxuwu']
-if not settings.configured:
-    settings.configure()
+settings.configure()
 for name in namelist:
     user = User.objects.create_user(name, '123@123.com', name)
